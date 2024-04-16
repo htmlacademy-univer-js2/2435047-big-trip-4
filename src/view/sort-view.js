7dc5b13cced4fe97369b6b7d5bfc86ab3d164a49
Andrew 61X Tiunov <astermiizek@gmail.com>
2024-04-16 21:32:10 +0500

    fix

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -4,7 +4,7 @@ import { SortType } from '../const.js';
 function createSortItems() {
   return Object.values(SortType).map((type) =>
     `<div class="trip-sort__item  trip-sort__item--${type}">
-      <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer">
+      <input id="sort-${type}" class="trip-sort__input  visually-hidden" data-sort-type="${type}" type="radio" name="trip-sort" value="sort-offer">
       <label class="trip-sort__btn" for="sort-${type}">${type}</label>
     </div>`
   ).join('');
@@ -38,6 +38,6 @@ export default class SortView extends AbstractView {
     }
 
     evt.preventDefault();
-    this.#handleSortTypeChange(evt.target.id.slice(5,));
+    this.#handleSortTypeChange(evt.target.dataset.sortType);
   };
 }
